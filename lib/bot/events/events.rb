f560945ffe926f020d4fde0cd4747a77d3f27400
Reaver01 <reaver_01@gmx.com>
2017-01-12 23:57:34 +0100

    Update 3
    #events.rb
    -change limiter to 30 seconds
    -make sure if player is at max level it doesn't try to give more levels

--- lib/bot/events/events.rb
+++ lib/bot/events/events.rb
@@ -11,37 +11,39 @@ module Events
 				else
 					oldtime = "2017-01-01 00:00:00 +0000"
 				end
-				if TimeDifference.between(oldtime, newtime).in_minutes > 2
+				if TimeDifference.between(oldtime, newtime).in_seconds > 30
 					$players[event.user.id.to_s]['xp'] += rand(15..25)
 					$players[event.user.id.to_s]['time'] = newtime
 				end
 			else
 				$players[event.user.id.to_s] = {'xp'=>0, 'level'=>0, 'hr'=>0, 'zenny'=>100, 'time'=>newtime, 'inv'=>{'0'=>1}}
 			end
-			if $players[event.user.id.to_s]['xp'] > $levels[$players[event.user.id.to_s]['level']+1]
-				zenny = (rand(0..9) * 10) + (rand(0..9) * 100) + (($players[event.user.id.to_s]['level'] / 4).floor * 1000)
-				$players[event.user.id.to_s]['level'] += 1
-				if $players[event.user.id.to_s].key?("messages")
-					if $players[event.user.id.to_s]['messages']
-						$bot.user(event.user.id.to_s).pm("Congratulations! You have leveled up to Level #{$players[event.user.id.to_s]['level']}\nYou have earned yourself #{zenny} Zenny and a few items you can trade or use!")
+			if $players[event.user.id.to_s]['level'] < 100
+				if $players[event.user.id.to_s]['xp'] > $levels[$players[event.user.id.to_s]['level']+1]
+					zenny = (rand(0..9) * 10) + (rand(0..9) * 100) + (($players[event.user.id.to_s]['level'] / 4).floor * 1000)
+					$players[event.user.id.to_s]['level'] += 1
+					if $players[event.user.id.to_s].key?("messages")
+						if $players[event.user.id.to_s]['messages']
+							$bot.user(event.user.id.to_s).pm("Congratulations! You have leveled up to Level #{$players[event.user.id.to_s]['level']}\nYou have earned yourself #{zenny} Zenny and a few items you can trade or use!")
+						end
 					end
-				end
-				$players[event.user.id.to_s]['zenny'] += zenny
-				numitems = rand(3..5)
-				newitems = []
-				numitems.times do
-					item = rand(0..($items.length-1))
-					newitems.push(item)
-					item = item.to_s
-					if $players[event.user.id.to_s]['inv'].key?(item)
-						$players[event.user.id.to_s]['inv'][item] += 1
-					else
-						$players[event.user.id.to_s]['inv'][item] = 1
+					$players[event.user.id.to_s]['zenny'] += zenny
+					numitems = rand(3..5)
+					newitems = []
+					numitems.times do
+						item = rand(0..($items.length-1))
+						newitems.push(item)
+						item = item.to_s
+						if $players[event.user.id.to_s]['inv'].key?(item)
+							$players[event.user.id.to_s]['inv'][item] += 1
+						else
+							$players[event.user.id.to_s]['inv'][item] = 1
+						end
 					end
-				end
-				if $players[event.user.id.to_s].key?("messages")
-					if $players[event.user.id.to_s]['messages']
-						$bot.user(event.user.id.to_s).pm.send_embed '', newItems(newitems, event.user.name.to_s)
+					if $players[event.user.id.to_s].key?("messages")
+						if $players[event.user.id.to_s]['messages']
+							$bot.user(event.user.id.to_s).pm.send_embed '', newItems(newitems, event.user.name.to_s)
+						end
 					end
 				end
 			end
