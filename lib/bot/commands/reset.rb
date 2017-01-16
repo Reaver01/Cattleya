ac227c14988a9ae8d73d0e544a10ea7303a617a9
TriZ0n <ts.trizon@gmail.com>
2017-01-16 13:32:34 -0600

    Untested -reset command.

--- lib/bot/commands/reset.rb
+++ lib/bot/commands/reset.rb
@@ -4,13 +4,13 @@ module Commands
 		command(
 				:reset,
 				bucket: :delay10,
-				description: "Displays time until gift/ticket reset",
+				description: "Resets player back to 0",
 				useage: "reset"
 		) do |event|
-			t2 = Time.now.to_i
-			t1 = Time.parse("19:00").to_i
-			if t1 > t2 then event.respond "#{Time.at(t1 - t2).strftime('**%H** hours **%M** minutes **%S** seconds')} left until the next gift/ticket reset"
-			else event.respond "#{Time.at(t1 + 86400 - t2).strftime('**%H** hours **%M** minutes **%S** seconds')} left until the next gift/ticket reset" end
+			if $players.key?(event.user.id.to_s)
+				$players[event.user.id.to_s] = {'xp'=>0, 'level'=>0, 'hr'=>0, 'zenny'=>100, 'time'=>newtime, 'inv'=>{'0'=>1}}
+				event.respond "**#{event.user.name}** has been reset."
+			end
 			puts "[#{event.timestamp.strftime("%d %a %y | %H:%M:%S")}] #{event.user.name}: CMD: reset"
 			nil
 		end
