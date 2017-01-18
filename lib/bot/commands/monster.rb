590a61fdcde89ae8eeba91d650ead456c2c4ba8b
Reaver01 <jawmac@gmail.com>
2017-01-18 11:36:05 +0100

    Create monster.rb

---
+++ lib/bot/commands/monster.rb
@@ -0,0 +1,18 @@
+module Commands
+	module Monster
+		extend Discordrb::Commands::CommandContainer
+		command(
+				:monster,
+				description: "responds with monster info",
+				useage: "monster"
+		) do |event|
+			if $curunst.has_key?(event.channel.id.to_s)
+				event.channel.send_embed '', monster($curunst[event.channel.id.to_s])
+			else
+				event.respond "There isn't a monster in this channel right now."
+			end
+			puts "[#{event.timestamp.strftime("%d %a %y | %H:%M:%S")}] #{event.user.name}: CMD: info"
+			nil
+		end
+	end
+end
