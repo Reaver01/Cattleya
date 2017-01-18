9a17dfb260f3a70f529bb30ac45e0be26e071881
Reaver01 <reaver_01@gmx.com>
2017-01-18 19:16:47 +0100

    add buy command

---
+++ lib/bot/commands/buy.rb
@@ -0,0 +1,29 @@
+module Commands
+	module Buy
+		extend Discordrb::Commands::CommandContainer
+		command(
+				:buy,
+				description: "Buys an item from the shop",
+				useage: "buy",
+				min_args: 2,
+				max_args: 3
+		) do |event, opt, num=1|
+			if num < 1
+				num = 1
+			end
+			num = num.to_i.round
+			opt = opt.to_i
+			if $players.has_key?(event.user.id.to_s)
+				if $players[event.user.id.to_s]['zenny'] < $items[opt-1]['price'] * num
+					event.respond "You don't have enough Zenny to purchase #{num} #{$items[opt-1]['name']}"
+				else
+					$players[event.user.id.to_s]['zenny'] -= $items[opt-1]['price'] * num
+					$players[event.user.id.to_s]['inv']["#{opt-1}"] += 1 * num
+					event.respond "**#{event.user.name}** purchased **#{num} #{$items[opt-1]['name']}"
+				end
+			end
+			puts "[#{event.timestamp.strftime("%d %a %y | %H:%M:%S")}] #{event.user.name}: CMD: inventory"
+			nil
+		end
+	end
+end
