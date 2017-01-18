15835ede4beacea7761c2f1a1bf80257b174415e
Reaver01 <reaver_01@gmx.com>
2017-01-18 21:21:04 +0100

    add random colors back

--- lib/bot/class/embeds.rb
+++ lib/bot/class/embeds.rb
@@ -1,7 +1,7 @@
 def embed(e_name, e_desc)
 	Discordrb::Webhooks::Embed.new(
 		author: { name: e_name },
-		color: 0xffffff,
+		color: rand(0xffffff),
 		description: e_desc,
 		timestamp: Time.now
 	)
