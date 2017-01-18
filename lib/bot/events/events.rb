8ed63edc6d0b78add09e9aef30f136018105b5c5
Reaver01 <reaver_01@gmx.com>
2017-01-18 22:07:40 +0100

    somehow deleted has_

--- lib/bot/events/events.rb
+++ lib/bot/events/events.rb
@@ -6,7 +6,7 @@ module Events
 		else
 			new_time = event.timestamp
 			if $players.key?(event.user.id.to_s)
-				if $players[event.user.id.to_s].key?('time')
+				if $players[event.user.id.to_s].has_key?('time')
 					old_time = $players[event.user.id.to_s]['time']
 				else
 					old_time = "2017-01-01 00:00:00 +0000"
