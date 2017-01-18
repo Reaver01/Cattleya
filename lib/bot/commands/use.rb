e5b243f8a2517b8d1c8ce5c2db40931c0a32ac0b
Reaver01 <reaver_01@gmx.com>
2017-01-18 22:41:44 +0100

    restore my pretty names for this file

--- lib/bot/commands/use.rb
+++ lib/bot/commands/use.rb
@@ -6,33 +6,33 @@ module Commands
 				description: "Use an item",
 				useage: "use <item> <user>"
 		) do |event, *item|
-			uname = item[-1]
-			itemsindexed = Hash[$items.map.with_index.to_a]
-			useditem = -1
-			if BOT.parse_mention(uname) !=nil
+			user_name = item[-1]
+			items_indexed = Hash[$items.map.with_index.to_a]
+			used_item = -1
+			if BOT.parse_mention(user_name) !=nil
 				#throw things at somebody
 				item = item.first item.size - 1
 				item = item.join(' ').titleize
-				threw = false
+				used = false
 				x = 0
 				begin
 					if $items[x]['name'] == item
-						threw = true
-						useditem = itemsindexed[$items[x]].to_s
-						thrownindex = x
+						used = true
+						used_item = items_indexed[$items[x]].to_s
+						used_index = x
 					end
 					x += 1
 				end while x < $items.length
-				if threw
-					if $players[event.user.id.to_s]['inv'].has_key?(useditem)
-						if $items[thrownindex]['throw']
+				if used
+					if $players[event.user.id.to_s]['inv'].has_key?(used_item)
+						if $items[used_index]['throw']
 							event.respond "**#{item}s** must be thrown!"
 						else
-							event.respond "**#{event.user.name}** used a **#{item}** on #{uname}!"
-							$players[event.user.id.to_s]['inv'][useditem] -= 1
+							event.respond "**#{event.user.name}** used a **#{item}** on #{user_name}!"
+							$players[event.user.id.to_s]['inv'][used_item] -= 1
 						end
-						if $players[event.user.id.to_s]['inv'][useditem] < 1
-							$players[event.user.id.to_s]['inv'] = $players[event.user.id.to_s]['inv'].without(useditem)
+						if $players[event.user.id.to_s]['inv'][used_item] < 1
+							$players[event.user.id.to_s]['inv'] = $players[event.user.id.to_s]['inv'].without(used_item)
 						end
 					else
 						event.respond "**#{event.user.name}** doesn't have any **#{item}s** to use!"
@@ -41,29 +41,29 @@ module Commands
 			else
 				#throw things at the ground
 				item = item.join(' ').titleize
-				threw = false
+				used = false
 				x = 0
 				begin
 					if $items[x]['name'] == item
-						threw = true
-						useditem = itemsindexed[$items[x]].to_s
-						thrownindex = x
+						used = true
+						used_item = items_indexed[$items[x]].to_s
+						used_index = x
 					end
 					x += 1
 				end while x < $items.length
-				if threw
-					if $players[event.user.id.to_s]['inv'].has_key?(useditem)
-						if $items[thrownindex]['throw']
+				if used
+					if $players[event.user.id.to_s]['inv'].has_key?(used_item)
+						if $items[used_index]['throw']
 							event.respond "**#{item}s** must be thrown!"
 						else
 							event.respond "**#{event.user.name}** used a **#{item}**!"
-							$players[event.user.id.to_s]['inv'][useditem] -= 1
+							$players[event.user.id.to_s]['inv'][used_item] -= 1
 						end
-						if $players[event.user.id.to_s]['inv'][useditem] < 1
-							$players[event.user.id.to_s]['inv'] = $players[event.user.id.to_s]['inv'].without(useditem)
+						if $players[event.user.id.to_s]['inv'][used_item] < 1
+							$players[event.user.id.to_s]['inv'] = $players[event.user.id.to_s]['inv'].without(used_item)
 						end
 						if $current_unstable.has_key?(event.channel.id.to_s)
-							if useditem == 'Shock Trap'
+							if used_item == 'Shock Trap'
 								if ['shock', 'both'].include? $current_unstable[event.channel.id.to_s]['trap']
 									$current_unstable[event.channel.id.to_s]['intrap'] = true
 									$current_unstable[event.channel.id.to_s]['traptime'] = Time.now
@@ -72,7 +72,7 @@ module Commands
 									event.respond "The #{$current_unstable[event.channel.id.to_s]['name']} can't be trapped by this type of trap!"
 								end
 							end
-							if useditem == 'Pitfall Trap'
+							if used_item == 'Pitfall Trap'
 								if ['pitfall', 'both'].include? $current_unstable[event.channel.id.to_s]['trap']
 									$current_unstable[event.channel.id.to_s]['intrap'] = true
 									$current_unstable[event.channel.id.to_s]['traptime'] = Time.now
