c51db36db68a4e23a6445953e3214955476fa565
Reaver01 <reaver_01@gmx.com>
2017-01-18 18:05:13 +0100

    fix hosting

--- lib/bot/commands/hosting.rb
+++ lib/bot/commands/hosting.rb
@@ -5,7 +5,7 @@ module Commands
 				:hosting,
 				bucket: :delay10
 		) do |event|
-			event.respond "Felyne is hosted on DigitalOcean Servers.\nHost your own bot/app with $10 credit using my referral link: <https://m.do.co/c/71922c060e60>\nThis will help me keep Felyne live as long as you keep using their hosting after the first $10 credit :D"
+			event.respond "This bot is hosted on DigitalOcean Servers.\nHost your own bot/app with $10 credit using my referral link: <https://m.do.co/c/71922c060e60>\nThis will help me keep Felyne live as long as you keep using their hosting after the first $10 credit :D"
 			puts "[#{event.timestamp.strftime("%d %a %y | %H:%M:%S")}] #{event.user.name}: CMD: hosting"
 			nil
 		end
