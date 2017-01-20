d45de0a2373ea7ade9a88e0ddbacdd316faf4948
Reaver01 <reaver_01@gmx.com>
2017-01-20 12:49:18 +0100

    add actual logger

--- lib/bot/class/logs.rb
+++ lib/bot/class/logs.rb
@@ -8,3 +8,15 @@ def command_log(command_name, user_name)
 	$logs['commands'][command_name] += 1
 	puts "[#{Time.now.strftime("%d %a %y | %H:%M:%S")}] #{user_name}: CMD: #{command_name}"
 end
+
+def mute_log(channel_id)
+	unless $logs.has_key?('muted')
+		$logs['muted'] = {}
+	end
+	unless $logs['muted'].has_key(channel_id.to_s)
+		$logs['muted'][channel_id.to_s] = [Time.now]
+	else
+		$logs['muted'][channel_id.to_s].push(Time.now)
+	end
+	puts "[#{Time.now.strftime("%d %a %y | %H:%M:%S")}] BOT has been muted in #{channel_id}"
+end
