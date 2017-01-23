64ebb56ec8ed38cee1236a4508ce3b092f9bb5a2
Reaver01 <reaver_01@gmx.com>
2017-01-23 20:58:23 +0100

    only put user id if it's an x

--- lib/bot/modules/addemoji.rb
+++ lib/bot/modules/addemoji.rb
@@ -2,6 +2,7 @@
 module Events
   extend Discordrb::EventContainer
   reaction_add(emoji: "\u274C") do |event|
+    next unless event.emoji.name == "\u274C"
     puts event.message.author
   end
 end
