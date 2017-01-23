26f71555e34c78eeee97c925fa0cf69342f109ea
Reaver01 <reaver_01@gmx.com>
2017-01-23 21:18:59 +0100

    don't need that puts

--- lib/bot/modules/addemoji.rb
+++ lib/bot/modules/addemoji.rb
@@ -4,7 +4,6 @@ module Events
   reaction_add(emoji: "\u274C") do |event|
     next unless event.emoji.name == "\u274C"
     next unless event.message.from_bot?
-    puts 'Reaction was placed on bot'
     event.message.delete
   end
 end
