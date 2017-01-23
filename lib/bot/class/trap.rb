8a3aad0abbf4b13e5eac22dd173730f2b51b2628
Reaver01 <jawmac@gmail.com>
2017-01-23 14:59:17 +0100

    Add debugger

--- lib/bot/class/trap.rb
+++ lib/bot/class/trap.rb
@@ -1,7 +1,9 @@
 def trap(event_channel, event_timestamp)
 	if $current_unstable.has_key?(event_channel.id.to_s)
-		if $current_unstable[event_channel.id.to_s].has_key?('traptime')		
+		if $current_unstable[event_channel.id.to_s].has_key?('traptime')
+		debug(4, "[TRAP] Monster has a traptime value")
 			if TimeDifference.between($current_unstable[event_channel.id.to_s]['traptime'], event_timestamp).in_minutes > 2
+				debug(6, "[TRAP] Monster has been in the trap for longer than 2 minutes | Releasing from trap.")
 				$current_unstable[event_channel.id.to_s]['intrap'] = false
 				$current_unstable[event_channel.id.to_s] = $current_unstable[event_channel.id.to_s].without('traptime')
 				begin
