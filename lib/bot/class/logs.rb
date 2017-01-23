5bd738ea1cf924586546d0fb2e1ecac3153dd0e8
Reaver01 <reaver_01@gmx.com>
2017-01-23 16:11:50 +0100

    unify logs

--- lib/bot/class/logs.rb
+++ lib/bot/class/logs.rb
@@ -6,7 +6,7 @@ def command_log(command_name, user_name)
 		$logs['commands'][command_name] = 0
 	end
 	$logs['commands'][command_name] += 1
-	puts "[#{Time.now.strftime("%d %a %y | %H:%M:%S")}] #{user_name}: CMD: #{command_name}"
+	puts "[#{Time.now.strftime("%d %a %y | %H:%M:%S")}][COMMAND] #{user_name}: #{command_name}"
 end
 
 def mute_log(channel_id)
@@ -18,7 +18,7 @@ def mute_log(channel_id)
 	else
 		$logs['muted'][channel_id.to_s].push(Time.now)
 	end
-	puts "[#{Time.now.strftime("%d %a %y | %H:%M:%S")}] BOT has been muted in #{channel_id}"
+	puts "[#{Time.now.strftime("%d %a %y | %H:%M:%S")}][MUTE] BOT has been muted in #{channel_id}"
 end
 
 def killed_log(channel_id, monster_name)
@@ -29,5 +29,5 @@ def killed_log(channel_id, monster_name)
 		$logs['monsters_killed'][monster_name] = 0
 	end
 	$logs['monsters_killed'][monster_name] += 1
-	puts "[#{Time.now.strftime("%d %a %y | %H:%M:%S")}] #{monster_name} has been killed in #{channel_id}"
+	puts "[#{Time.now.strftime("%d %a %y | %H:%M:%S")}][KILLED] #{monster_name} has been killed in #{channel_id}"
 end
