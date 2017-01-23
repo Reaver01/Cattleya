372292a581702b2414aa8413694d45b31708a835
Reaver01 <jawmac@gmail.com>
2017-01-23 10:26:37 +0100

    Update and rename levels.rb to onevent.rb

---
+++ lib/bot/modules/onevent.rb
@@ -0,0 +1,21 @@
+module Events
+	extend Discordrb::EventContainer
+	message do |event|
+		if event.message.channel.pm?
+			#does nothing
+		else
+			unless event.message.content.include?(PREFIX)
+				levels(event.user, event.timestamp)
+				damage(event.user, event.channel, event.timestamp)
+				trap(event.channel, event.timestamp)
+				if event.message.content.include? $hit
+					hit(event.user, event.channel)
+				end
+				if event.message.content.include? $anger
+					anger(event.channel)					
+				end
+				anger_check(event_channel, event_timestamp)
+			end
+		end
+	end
+end
