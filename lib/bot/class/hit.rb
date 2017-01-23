5c782201f6e1625d7a0a29c9ffe8bf525c9c636b
Reaver01 <jawmac@gmail.com>
2017-01-23 14:53:39 +0100

    Make debugging include user id

--- lib/bot/class/hit.rb
+++ lib/bot/class/hit.rb
@@ -1,6 +1,6 @@
 def hit(event_user, event_channel)
 	if $current_unstable.has_key?(event_channel.id.to_s)
-		debug(3, "[HIT] There is a monster in the channel.")
+		debug(3, "[HIT] There is a monster in #{event_channel.id}.")
 		if $current_unstable[event_channel.id.to_s].has_key?('intrap')
 			debug(5, "[HIT] Monster has intrap key.")
 			if $current_unstable[event_channel.id.to_s]['intrap']
@@ -15,25 +15,25 @@ def hit(event_user, event_channel)
 			trap_modifier = 1
 		end
 		unless $players.has_key?(event_user.id.to_s)
-			debug(19, "[HIT] Player doesn't have HR | Setting modifier to 0.")
+			debug(19, "[HIT] #{event_user.id} doesn't have HR | Setting modifier to 0.")
 			hr_modifier = 0
 		else
-			debug(22, "[HIT] Player has HR | Setting modifier.")
+			debug(22, "[HIT] #{event_user.id} has HR | Setting modifier.")
 			hr_modifier = $players[event_user.id.to_s]['hr']
 		end
 		damage_dealt = 0
 		if $current_unstable[event_channel.id.to_s].has_key?('is_dead')
 			debug(26, "[HIT] Monster has is_dead key.")
 			if $current_unstable[event_channel.id.to_s]['is_dead'].has_key?(event_user.id.to_s)
-				debug(28, "[HIT] is_dead has key for player.")
+				debug(28, "[HIT] is_dead has key for #{event_user.id}.")
 				unless $current_unstable[event_channel.id.to_s]['is_dead'][event_user.id.to_s]
-					debug(30, "[HIT] Player is not dead | calculating and dealing damage.")
+					debug(30, "[HIT] #{event_user.id} is not dead | calculating and dealing damage.")
 					damage_dealt = rand(0..(10 + hr_modifier)) * trap_modifier
 					damage_dealt = damage_dealt.round
 					$current_unstable[event_channel.id.to_s]['hp'] -= damage_dealt
 				end
 			else
-				debug(36, "[HIT] No is_dead key for player | calculating and dealing damage.")
+				debug(36, "[HIT] No is_dead key for #{event_user.id} | calculating and dealing damage.")
 				damage_dealt = rand(0..(10 + hr_modifier)) * trap_modifier
 				damage_dealt = damage_dealt.round
 				$current_unstable[event_channel.id.to_s]['hp'] -= damage_dealt
@@ -43,11 +43,11 @@ def hit(event_user, event_channel)
 		if $current_unstable[event_channel.id.to_s].has_key?('players')
 			debug(44, "[HIT] Monster has players array.")
 			if $current_unstable[event_channel.id.to_s]['players'].has_key?(event_user.id.to_s)
-				debug(46, "[HIT] Key exists for player | adding damage dealt and name to player arrays.")
+				debug(46, "[HIT] Key exists for #{event_user.id} | adding damage dealt and name to player arrays.")
 				$current_unstable[event_channel.id.to_s]['players'][event_user.id.to_s] += damage_dealt
 				$current_unstable[event_channel.id.to_s]['players2'][event_user.id.to_s] = event_user.name
 			else
-				debug(50, "[HIT] Key doesn't exist for player | Creating it and seting damage dealt.")
+				debug(50, "[HIT] Key doesn't exist for #{event_user.id} | Creating it and seting damage dealt.")
 				$current_unstable[event_channel.id.to_s]['players'][event_user.id.to_s] = damage_dealt
 				$current_unstable[event_channel.id.to_s]['players2'][event_user.id.to_s] = event_user.name
 			end
