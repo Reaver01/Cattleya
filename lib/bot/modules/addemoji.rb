6b86aab3ba19b519c08559aef1f96e05f1fa5e15
Reaver01 <reaver_01@gmx.com>
2017-01-23 21:02:23 +0100

    fix double quotes

--- lib/bot/modules/addemoji.rb
+++ lib/bot/modules/addemoji.rb
@@ -4,6 +4,6 @@ module Events
   reaction_add(emoji: "\u274C") do |event|
     next unless event.emoji.name == "\u274C"
     next unless event.message.from_bot?
-    puts "Reaction was placed on bot"
+    puts 'Reaction was placed on bot'
   end
 end
