e6238e451dfbf3d560d93746ab55e8ff34cdb99c
Reaver01 <reaver_01@gmx.com>
2017-01-24 16:42:38 +0100

    fixed error when no json files exist

--- lib/bot/class/loader.rb
+++ lib/bot/class/loader.rb
@@ -1,33 +1,35 @@
 def load_json(file_location)
   if File.exist?(file_location)
     begin
-      array = {}
-      array = JSON.parse File.read file_location
+      ar = {}
+      ar = JSON.parse File.read file_location
     rescue
-      array = {}
+      ar = {}
     end
   else
+    ar = {}
     puts "[#{Time.now.strftime('%d %a %y | %H:%M:%S')}][LOADER] No file #{file_location} to load!"
   end
   puts "[#{Time.now.strftime('%d %a %y | %H:%M:%S')}][LOADER] #{file_location} loaded!"
-  array
+  ar
 end
 
 def load_permissions(file_location)
   if File.exist?(file_location)
     begin
-      array = {}
-      array = JSON.parse File.read file_location
+      ar = {}
+      ar = JSON.parse File.read file_location
     rescue
-      array = {}
+      ar = {}
     end
   else
+    ar = {}
     puts '[#{Time.now.strftime("%d %a %y | %H:%M:%S")}][LOADER] You have not set up any permissions', '[LOADER] Please enter your user id to set admin permissions for your discord account'
     user_id = $stdin.gets.chomp.to_i
-    array[user_id] = { 'id' => user_id.to_i, 'lvl' => 999 }
-    File.open(file_location, 'w') { |f| f.write array.to_json }
+    ar[user_id] = { 'id' => user_id.to_i, 'lvl' => 999 }
+    File.open(file_location, 'w') { |f| f.write ar.to_json }
     puts "[#{Time.now.strftime('%d %a %y | %H:%M:%S')}][LOADER] Permissions saved!"
   end
   puts "[#{Time.now.strftime('%d %a %y | %H:%M:%S')}][LOADER] #{file_location} loaded!"
-  array
+  ar
 end
