300f6da2c1052a22f1f6b83fc4783a3374ea6834
reaver01 <reaver_01@gmx.com>
2017-01-27 23:07:39 +0100

    satisfy rubocop line length

--- lib/bot/class/damage.rb
+++ lib/bot/class/damage.rb
@@ -1,18 +1,20 @@
 def damage(user, channel, message, timestamp)
-  if $current_unstable.key?(channel.id.to_s)
+  channel_id = channel.id.to_s
+  user_id = user.id.to_s
+  if $current_unstable.key?(channel_id)
     if rand(0..9).zero?
-      if $current_unstable[channel.id.to_s].key?('players')
-        value = if $current_unstable[channel.id.to_s]['players'].key?(user.id.to_s)
-                  $current_unstable[channel.id.to_s]['players'][user.id.to_s]
+      if $current_unstable[channel_id].key?('players')
+        value = if $current_unstable[channel_id]['players'].key?(user_id)
+                  $current_unstable[channel_id]['players'][user_id]
                 else
                   0
                 end
-        if $players.key?(user.id.to_s)
+        if $players.key?(user_id)
           message.react('💥')
           damage_done = 0
-          if $current_unstable[channel.id.to_s].key?('is_dead')
-            if $current_unstable[channel.id.to_s]['is_dead'].key?(user.id.to_s)
-              unless $current_unstable[channel.id.to_s]['is_dead'][user.id.to_s]
+          if $current_unstable[channel_id].key?('is_dead')
+            if $current_unstable[channel_id]['is_dead'].key?(user_id)
+              unless $current_unstable[channel_id]['is_dead'][user_id]
                 damage_done = rand(0..value)
               end
             else
@@ -23,60 +25,64 @@ def damage(user, channel, message, timestamp)
           end
           damage_done = damage_done.round
           unless damage_done.zero?
-            if $players[user.id.to_s]['messages']
+            if $players[user_id]['messages']
               begin
-                BOT.user(user.id.to_s).pm(
+                BOT.user(user_id).pm(
                   "You have taken **#{damage_done} damage** from the " \
-                  "**#{$current_unstable[channel.id.to_s]['name']}**"
+                  "**#{$current_unstable[channel_id]['name']}**"
                 )
               rescue
-                mute_log(user.id.to_s)
+                mute_log(user_id)
               end
             end
-            unless $players[user.id.to_s].key?('max_hp')
-              new_hp = 500 + ($players[user.id.to_s]['level'] * 10)
-              $players[user.id.to_s]['max_hp'] = new_hp
-              $players[user.id.to_s]['current_hp'] = new_hp
+            unless $players[user_id].key?('max_hp')
+              new_hp = 500 + ($players[user_id]['level'] * 10)
+              $players[user_id]['max_hp'] = new_hp
+              $players[user_id]['current_hp'] = new_hp
             end
-            $players[user.id.to_s]['current_hp'] -= damage_done
-            if $players[user.id.to_s]['current_hp'] < 0
-              if $players[user.id.to_s]['messages']
+            $players[user_id]['current_hp'] -= damage_done
+            if $players[user_id]['current_hp'] < 0
+              if $players[user_id]['messages']
                 begin
-                  BOT.user(user.id.to_s).pm('You have taken too much damage! The felynes will take approximately 5 minutes to restore you to full power.')
+                  BOT.user(user_id).pm('You have taken too much damage! ' \
+                                       'The felynes will take approximately ' \
+                                       '5 minutes to restore you to full power')
                 rescue
-                  mute_log(user.id.to_s)
+                  mute_log(user_id)
                 end
               end
-              if $current_unstable[event_channel.id.to_s].key?('is_dead')
-                $current_unstable[event_channel.id.to_s]['is_dead'][user.id.to_s] = true
+              if $current_unstable[event_channel_id].key?('is_dead')
+                $current_unstable[event_channel_id]['is_dead'][user_id] = true
               else
-                $current_unstable[event_channel.id.to_s]['is_dead'] = { user.id.to_s => true }
+                $current_unstable[event_channel_id]['is_dead'] = {
+                  user_id => true
+                }
               end
-              $players[user.id.to_s]['death_time'] = event_timestamp
+              $players[user_id]['death_time'] = event_timestamp
             end
           end
         end
       end
     end
   end
-  if $players.key?(user.id.to_s)
-    if $players[user.id.to_s].key?('death_time')
+  if $players.key?(user_id)
+    if $players[user_id].key?('death_time')
       if TimeDifference.between(
-        $players[user.id.to_s]['death_time'], timestamp
+        $players[user_id]['death_time'], timestamp
       ).in_minutes > 5
-        $players[user.id.to_s] = $players[user.id.to_s].without(
+        $players[user_id] = $players[user_id].without(
           'death_time'
         )
-        if $current_unstable.key?(channel.id.to_s)
-          if $current_unstable[channel.id.to_s].key?('is_dead')
-            $current_unstable[channel.id.to_s]['is_dead'][user.id.to_s] = false
+        if $current_unstable.key?(channel_id)
+          if $current_unstable[channel_id].key?('is_dead')
+            $current_unstable[channel_id]['is_dead'][user_id] = false
           else
-            $current_unstable[channel.id.to_s]['is_dead'] = {
-              user.id.to_s => false
+            $current_unstable[channel_id]['is_dead'] = {
+              user_id => false
             }
           end
         end
-        $players[user.id.to_s]['current_hp'] = $players[user.id.to_s]['max_hp']
+        $players[user_id]['current_hp'] = $players[user_id]['max_hp']
       end
     end
   end
