498e545d39d2d05ef1f5e742e148440f05ad19bc
reaver01 <reaver_01@gmx.com>
2017-01-27 23:11:34 +0100

    remove debugging

--- lib/bot/class/anger.rb
+++ lib/bot/class/anger.rb
@@ -1,21 +1,15 @@
 def anger(event_channel)
   if $current_unstable.key?(event_channel.id.to_s)
-    debug(3, "[ANGER] #{event_channel.id} has a monster in it.")
     unless $current_unstable[event_channel.id.to_s].key?('angry')
-      debug(5, '[ANGER] Key for anger doesn\'t exist | Creating and setting false.')
       $current_unstable[event_channel.id.to_s]['angry'] = false
     end
     unless $current_unstable[event_channel.id.to_s]['angry']
-      debug(9, '[ANGER] Monster is not angry')
       if $current_unstable[event_channel.id.to_s].key?('anger')
-        debug(11, '[ANGER] Monster has anger key | Adding 1 to monster\'s anger level.')
         $current_unstable[event_channel.id.to_s]['anger'] += 1
       else
-        debug(14, '[ANGER] Monster doesn\'t have anger key | Creating it and setting to 1.')
         $current_unstable[event_channel.id.to_s]['anger'] = 1
       end
       if $current_unstable[event_channel.id.to_s]['anger'] > 50
-        debug(18, '[ANGER] Anger level has reached 50.')
         $current_unstable[event_channel.id.to_s]['angry'] = true
         $current_unstable[event_channel.id.to_s]['angertime'] = Time.now
         $current_unstable[event_channel.id.to_s]['anger'] = 0
