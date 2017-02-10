ccaaff1aaf7687f20c42be5b12bf15c2519d6d8a
reaver01 <reaver_01@gmx.com>
2017-02-10 17:22:24 +0100

    leading spaces

--- lib/bot/method/embeds.rb
+++ lib/bot/method/embeds.rb
@@ -109,21 +109,23 @@ def shop(arr)
 end
 
 def armor_piece(arr)
-  desc = "Monster: #{translate(arr[2])}\n" \
-         "Defense: #{arr[6]}\n" \
-         "[Attributes]\n"
-  desc += "   Water Res: #{arr[7]}\n" unless arr[7].to_i.zero?
-  desc += "   Fire Res: #{arr[8]}\n" unless arr[8].to_i.zero?
-  desc += "   Thunder Res: #{arr[9]}\n" unless arr[9].to_i.zero?
-  desc += "   Dragon Res: #{arr[10]}\n" unless arr[10].to_i.zero?
-  desc += "   Ice Res: #{arr[11]}\n" unless arr[11].to_i.zero?
-  desc += "   Mosaic Slots: #{arr[5]}\n" \
-          "[Passive Skills]\n"
-  desc += "   #{translate(arr[38])}: #{arr[39]}\n" unless arr[39].to_i.zero?
-  desc += "   #{translate(arr[40])}: #{arr[41]}\n" unless arr[41].to_i.zero?
-  desc += "   #{translate(arr[42])}: #{arr[43]}\n" unless arr[43].to_i.zero?
-  desc += "   #{translate(arr[44])}: #{arr[45]}\n" unless arr[45].to_i.zero?
-  desc += "   #{translate(arr[46])}: #{arr[47]}\n" unless arr[47].to_i.zero?
+  desc = "Monster: #{translate(arr[2])}\n"
+  desc += "Type: Blademaster\n" if arr[3].to_i.zero?
+  desc += "Type: Gunner\n" unless arr[3].to_i.zero?
+  desc += "Defense: #{arr[6]}\n" \
+          "**[Attributes]**\n"
+  desc += "\u00A0\u00A0\u00A0Water Res: #{arr[7]}\n" unless arr[7].to_i.zero?
+  desc += "\u00A0\u00A0\u00A0Fire Res: #{arr[8]}\n" unless arr[8].to_i.zero?
+  desc += "\u00A0\u00A0\u00A0Thunder Res: #{arr[9]}\n" unless arr[9].to_i.zero?
+  desc += "\u00A0\u00A0\u00A0Dragon Res: #{arr[10]}\n" unless arr[10].to_i.zero?
+  desc += "\u00A0\u00A0\u00A0Ice Res: #{arr[11]}\n" unless arr[11].to_i.zero?
+  desc += "\u00A0\u00A0\u00A0Mosaic Slots: #{arr[5]}\n" \
+          "**[Passive Skills]**\n"
+  desc += "\u00A0\u00A0\u00A0#{translate(arr[38])}: #{arr[39]}\n" unless arr[39].to_i.zero?
+  desc += "\u00A0\u00A0\u00A0#{translate(arr[40])}: #{arr[41]}\n" unless arr[41].to_i.zero?
+  desc += "\u00A0\u00A0\u00A0#{translate(arr[42])}: #{arr[43]}\n" unless arr[43].to_i.zero?
+  desc += "\u00A0\u00A0\u00A0#{translate(arr[44])}: #{arr[45]}\n" unless arr[45].to_i.zero?
+  desc += "\u00A0\u00A0\u00A0#{translate(arr[46])}: #{arr[47]}\n" unless arr[47].to_i.zero?
   thumbnail_color = Miro::DominantColors.new(
     "http://monsterhunteronline.in/images/item/#{arr[13]}.png"
   )
