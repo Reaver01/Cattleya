2e78165308f719ac12da2af32a7703bf2d93ab02
reaver01 <reaver_01@gmx.com>
2017-02-14 20:10:39 +0100

    Eval UPGRADE!!! IT'S OVER 9000

--- lib/bot/command/eval.rb
+++ lib/bot/command/eval.rb
@@ -9,11 +9,25 @@ module Commands
       help_available: false,
       permission_level: 999
     ) do |event, *code|
-      command_log('eval', event.user.name)
       begin
-        eval code.join(' ')
-      rescue StandardError => e
-        e.to_s
+        result = eval code.join(' ')
+        event.channel.send_embed do |e|
+          e.description = result.to_s
+          e.color = 0x00ff00
+        end
+      rescue => error
+        event.channel.send_embed do |e|
+          e.color = 0xff0000
+          e.description = "```#{error}```"
+          e.author = {
+            name: 'An error occurred!',
+            icon_url: 'http://emojipedia-us.s3.amazonaws.com/cache/f4/63/f463408675b9437b457915713b9af46c.png'
+          }
+          e.add_field(
+            name: 'Backtrace',
+            value: "```#{error.backtrace.join("\n")}```"
+          )
+        end
       end
     end
   end
