a049bb712c5d9f97baac51a018c2fd43c6dfa918
reaver01 <reaver_01@gmx.com>
2017-02-19 00:21:59 +0100

    remove message that doesn't even send

--- src/modules/commands/kill.rb
+++ src/modules/commands/kill.rb
@@ -10,9 +10,6 @@ module Bot
         help_available: false,
         permission_level: 999
       ) do |event|
-        # Sends a message to the channel
-        event.channel.send_temporary_message "Saving data and shutting down... I'll be back.", 5
-
         # Stops the bot
         BOT.stop
 
