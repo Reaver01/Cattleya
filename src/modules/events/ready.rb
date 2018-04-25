6162815fb556aa05f0a4ad42dce336af4aae98d8
Reaver01 <jawmac@gmail.com>
2018-04-25 13:11:54 -0600

    Update ready.rb

--- src/modules/events/ready.rb
+++ src/modules/events/ready.rb
@@ -10,6 +10,9 @@ module Bot
         # Set hit and anger variables
         $anger = %w(b l v y p w f g).sample
         $hit = %w(c m u d n t e h i o a s r).sample
+        
+        #Store leaderboard variable
+        $leaderboard = Database::Player.sort_by { |player| player.level }.reverse
 
         # Tell the console BOT is ready
         puts 'BOT Ready!'
